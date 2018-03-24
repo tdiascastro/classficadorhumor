bdf0826159590675888a3c96ce7e8269d05305c4
Castro <tdiascastro90@gmail.com>
2018-03-24 10:05:54 -0300

    COMITE INICIAL

---
+++ src/main/java/com/castro/classificadorhumor/ClassificadorhumorApplication.java
@@ -0,0 +1,12 @@
+package com.castro.classificadorhumor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassificadorhumorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassificadorhumorApplication.class, args);
+	}
+}
