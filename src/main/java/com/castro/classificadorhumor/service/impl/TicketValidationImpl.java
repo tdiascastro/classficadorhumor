2481772b5fbe26c47ffeae2e036666801fda0409
Castro <thiagocastro@finchsolucoes.com.br>
2018-03-26 10:04:43 -0300

    Definida formas de retornar pioridades nos métodos

--- src/main/java/com/castro/classificadorhumor/service/impl/TicketValidationImpl.java
+++ src/main/java/com/castro/classificadorhumor/service/impl/TicketValidationImpl.java
@@ -7,14 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 public class TicketValidationImpl implements TicketValidationService {
 
-    final private Pattern REGEX_WORDS= Pattern.compile("(reclamacao+)|(nao recebemos+)|(nao consigo acessar+)|(tentativa de contato+)|(cancelamento+)|(quero cancelar+)|(problema+)|(procon+)|(reclameaqui+)|(reclame+)");
+    final private Pattern REGEX_WORDS = Pattern.compile("(reclamacao+)|(nao recebemos+)|(nao consigo acessar+)|(tentativa de contato+)|(cancelamento+)|(quero cancelar+)|(problema+)|(procon+)|(reclameaqui+)|(reclame+)");
 
     private JsonManipulateService service;
 
@@ -31,11 +35,21 @@ public class TicketValidationImpl implements TicketValidationService {
     private List<Ticket> priorityAnalyse(final List<Ticket> tickets) {
         final List<Ticket> highPriorityTickets = new ArrayList<>();
         for (Ticket ticket : tickets) {
-            int interaction = ticket.getInteractions().size();
-            for (int i = 0; i <= interaction; i++){
-                ticket.getInteractions().get(i).getMessage();
+            final int interaction = ticket.getInteractions().size();
+            for (int i = 0; i <= interaction; i++) {
+                final Matcher matcher = REGEX_WORDS.matcher(ticket.getInteractions().get(i).getMessage());
+                if (matcher.find()){
+                    highPriorityTickets.add(ticket);
+                    break;
+                }
+                final long days = ChronoUnit.DAYS.between(ticket.getDateCreate(), ticket.getDateUpdate());
+                if (days > 30) {
+                    highPriorityTickets.add(ticket);
+                    break;
+                }
+
             }
         }
-        return null;
+        return highPriorityTickets;
     }
 }
